ca30467e75f5e4b258ac557067d41946a7b730fd
Lily Zhang <zlily@mit.edu>
2016-12-10 15:29:02 -0500

    added RGB color preserve and test images

--- preserve_color.py
+++ preserve_color.py
@@ -1,12 +1,12 @@
 import numpy
 from PIL import Image as im
 
-def preserve_color():	 
-	contentIm = im.open('hongkong.jpg')
+def preserve_color_YCC():	 
+	contentIm = im.open('boston.jpg')
 	contentIm = contentIm.convert("YCbCr")
 	contentImYUV = numpy.array(contentIm)
 
-	stylizedIm = im.open('hongkong-guernica.jpg')
+	stylizedIm = im.open('monet-boston.jpg')
 	stylizedIm = stylizedIm.convert("YCbCr")
 	stylizedImYUV = numpy.array(stylizedIm)
 
@@ -16,7 +16,34 @@ def preserve_color():
 
 	out = im.fromarray(contentImYUV, 'YCbCr')
 
-	out.save('out_preserve_color.jpg')
+	out.save('boston_YCC.jpg')
 	out.show()
 
-preserve_color()
+def preserve_color_RGB():
+	contentIm = im.open('boston.jpg')
+	contentIm = contentIm.convert("RGB")
+	contentArray = numpy.array(contentIm)
+
+	stylizedIm = im.open('monet-boston.jpg')
+	stylizedIm = stylizedIm.convert("RGB")
+	stylizedArray = numpy.array(stylizedIm)
+
+	CONTENT_WEIGHT = 0.5
+	STYLIZED_WEIGHT = 0.5
+	height = len(contentArray)
+	width = len(contentArray[0])
+	outArray = numpy.empty((height, width, 3))
+
+	for i in range(height):
+		for j in range(width):
+			weightedContent = CONTENT_WEIGHT*contentArray[i][j]
+			weightedStyle = STYLIZED_WEIGHT*stylizedArray[i][j]
+			outArray[i][j] = weightedContent + weightedStyle
+
+	out = im.fromarray(outArray.astype('uint8'), 'RGB')
+
+	out.save('boston_RGB.jpg')
+	out.show()
+
+preserve_color_YCC()
+preserve_color_RGB()
